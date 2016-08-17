87ad81a9f058b6e35bb1214b2b15fb22f2dbc182
the35 <majella2018@hsas-lehman.org>
2016-08-17 14:43:03 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+module.exports = router;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('finalproject.hbs');
+});
+router.get('/resources', function(req, res, next) {
+  res.render('rpage.hbs');
+});
+router.get('/ourbeautifulmap', function(req, res, next) {
+  res.render('UGHH.hbs');
+});
+router.get('/map', function(req, res, next) {
+  res.render('AHH.hbs');
+});
+router.get('/chat', function(req, res, next) {
+  res.render('chat.hbs');
+});
+
